728b3cc82649ff50a08ecee94518393f49b71b6a
vehancsenara <146476641+vehancsenara@users.noreply.github.com>
2025-12-01 21:30:07 +0100

    Add files via upload

--- Assignment 2/student_release_ex_2/solutions/svr.py
+++ Assignment 2/student_release_ex_2/solutions/svr.py
@@ -95,7 +95,101 @@ class EpsilonSVR:
         n, _ = X.shape
 
         # *****BEGINNING OF YOUR CODE (DO NOT DELETE THIS LINE)*****
-        raise NotImplementedError("Provide your solution here")
+        small_num=1e-5
+        #1. Optionally normalize X for stability.
+        if self.__normalize:
+            self.__norm=np.max(np.linalg.norm(X,axis=1))
+            
+            if self.__norm<small_num:
+                self.__norm=1.0
+            X_normed=X/self.__norm 
+        else:
+                self.__norm=1.0
+                X_normed=X.copy()
+        self.__training_X=X_normed
+
+        #2. Build the kernel Gram matrix K(X,X')
+        K=self._kernel(X_normed, X_normed)
+        
+        #3. Formulate the dual quadratic program in variables z = [alpha; alpha*].
+        #P Matrix (Quadratic part) =>(alpha - alpha*)^T K (alpha - alpha*)
+        P_block=np.block([[K, -K], [-K, K]])
+        P=cvxopt.matrix(P_block)
+        #q vector (Linear part) q_a=alpha, q_b=alpha*
+        ones=np.ones(n)
+        q_a=self.epsilon*ones-y
+        q_b=self.epsilon*ones+y
+        #make them column vector
+        q=cvxopt.matrix(np.hstack([q_a, q_b]), (2*n,1))
+        #Set up equality constraint Az=b A=[1,-1]*z=0
+        A_array=np.hstack([ones, -ones])
+        b=cvxopt.matrix(0.0)
+        A=cvxopt.matrix(A_array,(1,2*n))
+        #Set up inequality constraint Gz<h 0<=a_i<=C, 0<=b_i<=C
+        #G matrix -->(4n x 2n)
+        Is=np.eye(n)
+        zeros=np.zeros((n,n))
+        G_array=[np.hstack([-Is, zeros]),np.hstack([ Is, zeros]),np.hstack([zeros, -Is]),np.hstack([zeros,  Is])]
+        G=cvxopt.matrix(np.vstack(G_array))
+        #set up the h vector (4nby1)
+        h_c=self.C*ones
+        h_rows=np.hstack([np.zeros(n), h_c, np.zeros(n), h_c])
+        h=cvxopt.matrix(h_rows,(4*n,1))
+
+        #4. Solve with a QP solver (e.g. cvxopt).
+        solution = cvxopt.solvers.qp(P, q, G, h, A, b)
+        #5. Extract alpha, alpha*, coefficients (alpha - alpha*), and identify support vectors.
+        dual_var=np.array(solution['x']).ravel()
+        self.__a = dual_var[:n] # a
+        self.__a_star = dual_var[n:] # b
+
+        #Clipping noise near the boundaries
+        self.__a = np.clip(self.__a, 0, self.C)
+        self.__a_star = np.clip(self.__a_star, 0, self.C)
+        self.__a[self.__a < small_num] = 0.0
+        self.__a_star[self.__a_star < small_num] = 0.0
+        self.__a[self.__a > self.C - small_num] = self.C
+        self.__a_star[self.__a_star > self.C - small_num] = self.C
+
+        # Calculate coefficients + support mask
+        self.__coef = self.__a - self.__a_star
+        self.__support_mask = (np.abs(self.__coef) > small_num)
+
+        # 6. Compute bias b from conditions
+         # Free Support Vectors (fsvs) where 0 < a_i < C or 0 < b_i < C
+        fsv_a = (self.__a > small_num) & (self.__a < self.C - small_num)
+        fsv_b = (self.__a_star > small_num) & (self.__a_star < self.C - small_num)
+        
+        # A sample is an fsv if one of its multipliers is strictly between 0 and C
+        fsv_idxs = np.where(fsv_a | fsv_b)[0]
+        
+        if len(fsv_idxs) > 0:
+            # Calculate K * self.__coef (K * (a - b))
+            K_w = K @ self.__coef
+            
+            bestimates = []
+            
+            for k in fsv_idxs:
+                # If 0 < a_i < C: y_i - f(x_i) = epsilon  -->  biaas = y_i - epsilon - (K * w)_i
+                if fsv_a[k]:
+                    bestimates.append(y[k] - self.epsilon - K_w[k])
+                # If 0 < b_i < C: f(x_i) - y_i = epsilon  -->  bias = y_i + epsilon - (K * w)_i
+                elif fsv_b[k]:
+                    bestimates.append(y[k] + self.epsilon - K_w[k])
+            
+            if bestimates:
+                self.__bias = np.mean(bestimates)
+            else:
+                self.__bias = 0.0 
+        else:
+            # Fallback if no Free SVs found 
+            print("No Free Support Vectors found!!!")
+            # If no boundary than bias is zero
+            self.__bias = 0.0 
+
+
+
+
         # *****END OF YOUR CODE (DO NOT DELETE THIS LINE)*****
         
         return self
@@ -128,7 +222,26 @@ class EpsilonSVR:
         X = np.asarray(X, dtype=np.float64)
 
         # *****BEGINNING OF YOUR CODE (DO NOT DELETE THIS LINE)*****
-        raise NotImplementedError("Provide your solution here")
+        #Checking the normalization factor 
+        if self.__norm != 1.0:
+            X_scaled = X / self.__norm
+        else:
+            X_scaled = X.copy()
+            
+        # 2. Compute test Kernel matrix (K(X_train, X_test))
+        K_test = self._k_train_test(X_scaled) 
+        
+        # 3. Compute predictiom
+        
+        # Filter K_test to only include rows corresponding to SVs
+        K_sv = K_test[self.__support_mask, :]
+        
+        # Filter coefficients to only include non-zero ones
+        coef_sv = self.__coef[self.__support_mask]
+        
+        # Calculate the weihgted sum of kernels
+        y_pred = np.dot(coef_sv, K_sv) + self.__bias
+
         # *****END OF YOUR CODE (DO NOT DELETE THIS LINE)*****
 
         return y_pred
